895d15e9d9eac1c4b17c7077bfeea5ee0d966b3b
nichoth <nichoth@gmail.com>
2020-11-23 13:02:08 -0800

    rm test files

--- functions/a.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async function (ev, ctx, cb) {
-    var req = JSON.parse(ev.body)
-    console.log('req', req)
-    cb(null, {
-        statusCode: 200,
-        body: 'ok'
-    })
-}
