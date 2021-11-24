6a3d18329a5ce3549ce579fab24b3960323d46b4
nichoth <nichoth@gmail.com>
2021-11-24 15:33:03 -0800

    WIP factor out browser stuff b/c import

--- util.js
+++ util.js
@@ -3,8 +3,6 @@ var stringify = require('json-stable-stringify')
 var isCanonicalBase64 = require('is-canonical-base64')
 // var feedIdRegex = isCanonicalBase64('@', '.(?:sha256|ed25519)', 32)
 var isEncryptedRx = isCanonicalBase64('','\\.box.*')
-import utils from "keystore-idb/lib/utils.js"
-import * as uint8arrays from "uint8arrays"
 // import * as ucan from 'ucans'
 
 function clone (obj) {
@@ -128,44 +126,6 @@ function isInvalidShape (msg) {
     return isInvalidContent(msg.content)
 }
 
-function publicKeyToDid (publicKey, type) {
-    const pubKeyBuf = utils.base64ToArrBuf(publicKey)
-  
-    // Prefix public-write key
-    const prefix = magicBytes(type)
-    if (prefix === null) {
-      throw new Error(`Key type '${type}' not supported`)
-    }
-  
-    const prefixedBuf = utils.joinBufs(prefix, pubKeyBuf)
-  
-    // Encode prefixed
-    var arr = new Uint8Array(prefixedBuf)
-    return BASE58_DID_PREFIX + uint8arrays.toString(arr, "base58btc")
-}
-
-const EDWARDS_DID_PREFIX = new Uint8Array([ 0xed, 0x01 ])
-const BLS_DID_PREFIX = new Uint8Array([ 0xea, 0x01 ])
-const RSA_DID_PREFIX = new Uint8Array([ 0x00, 0xf5, 0x02 ])
-const BASE58_DID_PREFIX = "did:key:z"
-
-const KeyType = {
-    RSA: "rsa",
-    Edwards: "ed25519",
-    BLS: "bls12-381"
-}
-
-/**
- * Magic bytes.
- */
-function magicBytes (keyType) {
-    switch (keyType) {
-        case KeyType.Edwards: return EDWARDS_DID_PREFIX
-        case KeyType.RSA: return RSA_DID_PREFIX
-        case KeyType.BLS: return BLS_DID_PREFIX
-        default: return null
-    }
-}
 
 var base64 = {
     urlEncode: function urlEncode(str) {
@@ -246,24 +206,6 @@ function didToPublicKey(did, encoding) {
     }
 }
 
-/**
- * Convert a DID (did:key) to the public key in bytes
- */
- export function didToPublicKeyBytes(did) {
-    if (!did.startsWith(BASE58_DID_PREFIX)) {
-        throw new Error("Please use a base58-encoded DID formatted `did:key:z...`")
-    }
-  
-    const didWithoutPrefix = did.slice(BASE58_DID_PREFIX.length)
-    const magicBytes = uint8arrays.fromString(didWithoutPrefix, "base58btc")
-    const { keyBytes, type } = parseMagicBytes(magicBytes)
-  
-    return {
-        publicKey: keyBytes,
-        type
-    }
-}
-
 
 module.exports = {
     clone,
@@ -272,8 +214,6 @@ module.exports = {
     hash,
     isInvalidShape,
     isString,
-    publicKeyToDid,
-    magicBytes,
     encodeHeader,
     makeUrlUnsafe,
     verifySignedData,
