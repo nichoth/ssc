6cc1507743cb5ecb9adc8841d1926f92e504233d
nichoth <nichoth@gmail.com>
2022-04-02 23:20:13 -0700

    WIP

--- test/web/ucan.js
+++ test/web/ucan.js
@@ -89,7 +89,7 @@ function hermesCaps (chained) {
 }
 
 
-// var origUcan
+var origUcan
 test('create a UCAN with write capabilities', t => {
     /** did:key:z6Mkk89bC3JrVqKie71YEcc5M1SMVxuCgNx6zLZ8SYJsxALi */
     // const alice = ucan.EdKeypair.fromSecretKey("U+bzp2GaFQHso587iSFWPSeCzbSfn/CbNHEz7ilKRZ1UQMmMS7qq4UhTzKn3X9Nj/4xgrwa+UqhMOeo4Ki8JUw==")
@@ -113,28 +113,28 @@ test('create a UCAN with write capabilities', t => {
             proofs: [ucan.encode(mockServerUcan)]
         })
             .then(userUcan => {
-                // origUcan = userUcan
+                origUcan = userUcan
 
                 console.log('*user ucan*', userUcan)
 
-                Chained.fromToken(ucan.encode(userUcan))
-                    .then(chained => {
-                        t.ok(chained, 'should created chained version')
-                        console.log('caps', Array.from(hermesCaps(chained)))
-                        t.end()
-                    })
+                Promise.all([
+                    Chained.fromToken(ucan.encode(userUcan))
+                        .then(chained => {
+                            t.ok(chained, 'should created chained version')
+                            console.log('caps', Array.from(hermesCaps(chained)))
+                        }),
+
+                    // token.validate(ucan.encode(userUcan)).then(parsed => {
+                    //     console.log('*****parsed ucan', parsed)
+                    // })
+                ])
+                    .then(() => t.end())
                     .catch(err => {
-                        console.log('**errrrrrr**', err)
+                        console.log('*errrrr*', err)
+                        t.fail(err)
                         t.end()
                     })
 
-                // token.validate(ucan.encode(userUcan)).then(parsed => {
-                //     console.log('*****parsed ucan', parsed)
-                //     t.end()
-                // })
-                // .catch(err => {
-                //     console.log('errrrrrr', err)
-                // })
                 // console.log('****usss', ucan.issuer())
                 // ucan.isValid(userUcan).then(val => {
             //         var root = ucan.rootIssuer(ucan.encode(userUcan))
@@ -171,78 +171,85 @@ test('create a UCAN with write capabilities', t => {
 })
 
 
+// test('create a second ucan, for another device', t => {
+//     alice.getKeypair().then(keypair => {
+//         ucan.EdKeypair.create().then(deviceTwo => {
+//             ucan.build({
+//                 audience: deviceTwo.did(),
+//                 // issuer: alice.getKeypair(),
+//                 issuer: keypair,
+//                 capabilities: [{ hermes: 'member' }],
+//                 proofs: [ ucan.encode(origUcan) ]
+//             })
+//                 .then(_ucan => {
+//                     console.log('***ucan 2***', _ucan)
+//                     t.end()
+//                 })
+//                 .catch(err => {
+//                     console.log('*err*', err)
+//                     t.fail(err)
+//                     t.end()
+//                 })
+//         })
+//     })
+//     .catch(err => {
+//         console.log('errrrrrrrrrr', err)
+//         t.end()
+//     })
+// })
 
 
 
-// // // test('create a second ucan, for another device', t => {
-// // //     ucan.EdKeypair.create().then(deviceTwo => {
-// // //         ssc.getDidFromKeys(deviceTwo).then(did => {
-// // //             ucan.build({
+
+// // // var myUcan
+// // // test('create a ucan', async t => {
+// // //     t.plan(3)
+
+// // //     const issuerKeypair = await ucan.EdKeypair.create()
+
+// // //     var _did
+
+// // //     return ssc.getDidFromKeys(ks)
+// // //         .then(did => {
+// // //             _did = did
+
+// // //             return ucan.build({
 // // //                 audience: did,
-// // //                 issuer: ks,
-// // //                 capabilities: [{ hermes: 'member' }],
-// // //                 proof: ucan.encode(origUcan)
+// // //                 // issuer is a priv/pub keypair because the ucan is signed by
+// // //                 // the issuer
+// // //                 issuer: issuerKeypair,
+// // //                 // facts: [],
+// // //                 lifetimeInSeconds: 60 * 60 * 24, // UCAN expires in 24 hours
+// // //                 capabilities: [
+// // //                     {
+// // //                         "wnfs": "boris.fission.name/public/photos/",
+// // //                         "cap": "OVERWRITE"
+// // //                     }
+// // //                 ],
+// // //                 // proof: 'foo'
+// // //                 proof: null
 // // //             })
-// // //                 .then(_ucan => {
-// // //                     console.log('***ucan 2***', _ucan)
-// // //                     t.end()
-// // //                 })
 // // //         })
-// // //     })
+// // //         .then(_ucan => {
+// // //             myUcan = _ucan
+// // //             t.ok(myUcan, 'make a ucan')
+// // //             t.equal(_ucan.payload.att[0].wnfs,
+// // //                 "boris.fission.name/public/photos/",
+// // //                 'should set att to capability')
+// // //             t.equal(_ucan.payload.aud, _did,
+// // //                 'should put the audience did in the ucan')
+// // //         })
 // // // })
 
-
-
-
-// // var myUcan
-// // test('create a ucan', async t => {
-// //     t.plan(3)
-
-// //     const issuerKeypair = await ucan.EdKeypair.create()
-
-// //     var _did
-
-// //     return ssc.getDidFromKeys(ks)
-// //         .then(did => {
-// //             _did = did
-
-// //             return ucan.build({
-// //                 audience: did,
-// //                 // issuer is a priv/pub keypair because the ucan is signed by
-// //                 // the issuer
-// //                 issuer: issuerKeypair,
-// //                 // facts: [],
-// //                 lifetimeInSeconds: 60 * 60 * 24, // UCAN expires in 24 hours
-// //                 capabilities: [
-// //                     {
-// //                         "wnfs": "boris.fission.name/public/photos/",
-// //                         "cap": "OVERWRITE"
-// //                     }
-// //                 ],
-// //                 // proof: 'foo'
-// //                 proof: null
-// //             })
-// //         })
-// //         .then(_ucan => {
-// //             myUcan = _ucan
-// //             t.ok(myUcan, 'make a ucan')
-// //             t.equal(_ucan.payload.att[0].wnfs,
-// //                 "boris.fission.name/public/photos/",
-// //                 'should set att to capability')
-// //             t.equal(_ucan.payload.aud, _did,
-// //                 'should put the audience did in the ucan')
-// //         })
-// // })
-
-// // test('is the ucan valid?', t => {
-// //     t.plan(1)
-// //     ucan.isValid(myUcan)
-// //         .then(valid => {
-// //             t.equal(valid, true, 'should be a valid ucan')
-// //             t.end()
-// //         })
-// //         .catch (err => {
-// //             t.error(err, 'should not throw')
-// //             t.end()
-// //         })
-// // })
+// // // test('is the ucan valid?', t => {
+// // //     t.plan(1)
+// // //     ucan.isValid(myUcan)
+// // //         .then(valid => {
+// // //             t.equal(valid, true, 'should be a valid ucan')
+// // //             t.end()
+// // //         })
+// // //         .catch (err => {
+// // //             t.error(err, 'should not throw')
+// // //             t.end()
+// // //         })
+// // // })
