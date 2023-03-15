7547f5b5ca67537e006631edd54d60d0307ac6fe
nick thomas <nichoth@gmail.com>
2023-03-15 13:31:59 -0700

    use ?? instead of ||

--- test/create-merkle-list.js
+++ test/create-merkle-list.js
@@ -19,7 +19,7 @@ test('create a merkle list', async function (t) {
     // this is bad because the async reduce is confusing
     var list = await arr.reduce(async function (acc, val) {
         return acc.then(async _acc => {
-            var prev = (_acc[_acc.length - 1] || null)
+            var prev = (_acc[_acc.length - 1] ?? null)
             var msg = await ssc.createMsg(alice.keys, prev, {
                 type: 'test',
                 text: val
